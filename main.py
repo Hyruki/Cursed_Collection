87182e9a96b8c64fc11d4dd17194bc353926a23a
Hyruki <link4716@hotmail.com>
2024-11-29 09:50:36 +0100

    Ajout des premiers systèmes de déplacement

--- main.py
+++ main.py
@@ -1,7 +1,44 @@
-from utils import * 
+import pyxel
 
-# Affichage page en 1920x1080 (FullScreen)
-pyxel.init(1920, 1080, "The Darkness Lord")
+pyxel.init(256, 256, "The Darkness Lord")
+
+player_anim = {'up' : (16, 0, 16, 16, 0), 
+               'down' : (0, 0, 16, 16, 0), 
+               'right' : (32, 0, 16, 16, 0), 
+               'left' : (48, 0, 16, 16, 0)}
+
+curent_anim = player_anim['down']
+
+coordinates = [10, 10]
+
+
+def move_player():
+    global coordinates, curent_anim
+    
+    if pyxel.btn(pyxel.KEY_UP):
+        coordinates[1] -= 1
+        if not pyxel.btn(pyxel.KEY_SPACE):
+            curent_anim = player_anim['up']
+    if pyxel.btn(pyxel.KEY_DOWN):
+        coordinates[1] += 1
+        if not pyxel.btn(pyxel.KEY_SPACE):
+            curent_anim = player_anim['down']
+    if pyxel.btn(pyxel.KEY_LEFT):
+        coordinates[0] -= 1
+        if not pyxel.btn(pyxel.KEY_SPACE):
+            curent_anim = player_anim['left']
+    if pyxel.btn(pyxel.KEY_RIGHT):
+        coordinates[0] += 1
+        if not pyxel.btn(pyxel.KEY_SPACE):
+            curent_anim = player_anim['right']
+
+
+
+
+pyxel.fullscreen = True
+
+# Chargement des dessins
+pyxel.load('sprites.pyxres')
 
 # Fonction UPDATE qui execute à chaque FRAME
 def update():
@@ -9,9 +46,18 @@ def update():
     if pyxel.btn(pyxel.KEY_ESCAPE):
         pyxel.quit()
 
+    move_player()
+
+
 # Fonction DRAW qui dessine à chaque FRAME
 def draw():
-    pass
+    pyxel.cls(0)
+
+
+    player = pyxel.blt(coordinates[0], coordinates[1], 0, curent_anim[0], curent_anim[1], curent_anim[2], curent_anim[3], curent_anim[4]) # Ajout du personnage de TEST
+
+    print(curent_anim)
+
 
 # Execution du jeu en boucle
 pyxel.run(update, draw)
--- utils.py
+++ utils.py
@@ -6,4 +6,33 @@ import pyxel
 # L'ESPACE DE TRAVAIL
 # ============================================================================
 
+# Affichage page en 1920x1080 (FullScreen)
+pyxel.init(256, 256, "The Darkness Lord")
+
+player_anim = {'up' : (16, 0, 16, 16, 0), 
+               'down' : (0, 0, 16, 16, 0), 
+               'right' : (32, 0, 16, 16, 0), 
+               'left' : (48, 0, 16, 16, 0)}
+
+curent_anim = player_anim['down']
+
+coordinates = [10, 10]
+
+
+def move_player():
+    global coordinates, curent_anim
+    
+    if pyxel.btn(pyxel.KEY_UP):
+        coordinates[1] -= 1
+        curent_anim = player_anim['up']
+    if pyxel.btn(pyxel.KEY_DOWN):
+        coordinates[1] += 1
+        curent_anim = player_anim['down']
+    if pyxel.btn(pyxel.KEY_LEFT):
+        coordinates[0] -= 1
+        curent_anim = player_anim['left']
+    if pyxel.btn(pyxel.KEY_RIGHT):
+        coordinates[0] += 1
+        curent_anim = player_anim['right']
+
 
